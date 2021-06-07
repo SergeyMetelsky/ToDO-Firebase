9bc2d7b1dbee0fc083f33f1387acdeff115a4774
Metelsky <n_grey@mail.ru>
2021-06-07 13:33:29 +0300

    Creating tasks screen

---
+++ ToDo_Firebase/Controllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo_Firebase
+//
+//  Created by Sergey on 6/7/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+    }
+}
+
