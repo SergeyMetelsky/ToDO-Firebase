60510e156c4a8d18949b88c2caeb4faf88e121e5
Metelsky <n_grey@mail.ru>
2021-06-09 14:10:31 +0300

    Added Authentication

--- ToDo_Firebase/Controllers/LoginViewController.swift
+++ ToDo_Firebase/Controllers/LoginViewController.swift
@@ -6,18 +6,37 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var warnLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    let segueIdentifire = "tasksSegue"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+        warnLabel.alpha = 0
+        
+        Auth.auth().addIDTokenDidChangeListener { [weak self] (auth, user) in
+            if user != nil {
+                self?.performSegue(withIdentifier: (self?.segueIdentifire)!, sender: nil)
+            }
+        }
+    }
+    
+    // очистка полей после выхода из профиля
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        emailTextField.text = ""
+        passwordTextField.text = ""
     }
     
     @objc func keyboardDidShow(notification: Notification) {
@@ -30,11 +49,55 @@ class LoginViewController: UIViewController {
     @objc func keyboardDidHide() {
         (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
-
+    
+    func displayWarningLabel(withText text: String) {
+        warnLabel.text = text
+        
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseInOut]) {
+            [weak self] in
+            self?.warnLabel.alpha = 1
+        } completion: { [weak self] complete in
+            self?.warnLabel.alpha = 0
+        }
+    }
+    
     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Info is incorrect")
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] (user, error) in
+            if error != nil {
+                self?.displayWarningLabel(withText: "Error occured")
+                return
+            }
+            
+            if user != nil {
+                self?.performSegue(withIdentifier: self!.segueIdentifire, sender: nil)
+                return
+            }
+            self?.displayWarningLabel(withText: "No such user")
+        }
     }
     
     @IBAction func registerButtonPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text, email != "", password != "" else {
+            displayWarningLabel(withText: "Info is incorrect")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if error == nil {
+                if user != nil {
+                    self.performSegue(withIdentifier: self.segueIdentifire, sender: nil)
+                } else {
+                    print ("user is not created")
+                }
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
     }
 }
+
 
--- ToDo_Firebase/Controllers/TasksViewController.swift
+++ ToDo_Firebase/Controllers/TasksViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
+import Firebase
 
 class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.backgroundColor = .clear
@@ -24,5 +25,13 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func signOutButtonPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error.localizedDescription)
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
